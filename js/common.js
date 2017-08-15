dd4eed59491dd92a3b7f2fc27635471daae165e8
Alex2033 <XoxlofAlexandr@yandex.ru>
2017-08-15 15:11:30 +0300

    Add files via upload

--- js/common.js
+++ js/common.js
@@ -9,17 +9,26 @@
 });
 
 $(document).ready(function(){
-	var touch = $('.touch-menu');
-	var menu = $('.main-nav');
-		 
-	$(touch).on('click', function(e) {
-		e.preventDefault();
-		menu.slideToggle();
-	});
-	$(window).resize(function(){
-		var wid = $(window).width();
-		if(wid > 760 && menu.is(':hidden')) {
-			menu.removeAttr('style');
-		        }
-		});
+  // Add smooth scrolling to all links
+  $("a").on('click', function(event) {
+
+    // Make sure this.hash has a value before overriding default behavior
+    if (this.hash !== "") {
+      // Prevent default anchor click behavior
+      event.preventDefault();
+
+      // Store hash
+      var hash = this.hash;
+
+      // Using jQuery's animate() method to add smooth page scroll
+      // The optional number (800) specifies the number of milliseconds it takes to scroll to the specified area
+      $('html, body').animate({
+        scrollTop: $(hash).offset().top
+      }, 800, function(){
+   
+        // Add hash (#) to URL when done scrolling (default click behavior)
+        window.location.hash = hash;
+      });
+    } // End if
+  });
 });
